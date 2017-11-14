05a943ef70ed577af3f5e9580feb511aeed3f0ac
kesha <kesha@DESKTOP-MS33R55>
2017-11-14 12:41:47 -0800

    Lab 11 commit

--- src/main/java/Sorting.java
+++ src/main/java/Sorting.java
@@ -17,7 +17,7 @@ public class Sorting {
     private static final int SORT_INCREMENT = 10000;
 
     /** Total number of values to try. */
-    private static final int TOTAL_SORT_VALUES = 100;
+    private static final int TOTAL_SORT_VALUES = 20;
 
     /** Total data size. */
     private static final int TOTAL_INTEGER_VALUES = 1000000;
@@ -29,17 +29,68 @@ public class Sorting {
      * @return the sorted array, or null on failure
      */
     static int[] bubbleSort(final int[] array) {
-        return null;
+        Boolean swap = false;
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            if (array[i] < array[i - 1]) {
+                array[i] = array[i - 1];
+                array[i - 1] = temp;
+                swap = true;
+            }
+        }
+        if (swap) {
+            return bubbleSort(array);
+        }
+        return array;
     }
 
     /**
      * Selection sort.
      *
-     * @param array unsorted input array
+     * @param input input array
      * @return the sorted array, or null on failure
      */
-    static int[] selectionSort(final int[] array) {
-        return null;
+    static int[] selectionSort(final int[] input) {
+        int[] array = input;
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = findMin(array, i, array.length - 1);
+            array = swap(array, i, minIndex);
+        }
+        return array;
+    }
+
+    /**
+     * Find's minimum value's index.
+     *
+     * @param data  input array
+     * @param lo smallest index
+     * @param hi largest index
+     * @return the index of minimum value
+     */
+    public static int findMin(final int[] data, final int lo, final int hi) {
+        int min = lo;
+        for (int i = lo; i < hi; i++) {
+            if (data[i] < min) {
+                min = i;
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Selection sort.
+     *
+     * @param input input array
+     * @param x value to swap with y
+     * @param y value to swap with x
+     * @return the sorted array, or null on failure
+     */
+    public static int[] swap(final int[] input, final int x, final int y) {
+        int[] data = input;
+        int temp = data[x];
+        data[x] = data[y];
+        data[y] = temp;
+        return data;
     }
 
     /**
